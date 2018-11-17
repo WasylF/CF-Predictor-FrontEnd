f3a9316b46d9576afa7b2247dae71311c860b9c3
Vasyl Franchuk <WasylF@gmail.com>
2018-11-17 15:42:28 +0200

    dynamically get contest list

---
+++ src/main/webapp/contestSelector.js
@@ -0,0 +1,45 @@
+function getContestList(callback) {
+    var server = "http://codeforces.com/api/contest.list?gym=false";
+    $.getJSON(server, function (data) {
+        var contests = [];
+
+        for (var i = 0; i < data.result.length; i++) {
+            var id = data.result[i].id;
+            var name = data.result[i].name;
+            var startTime = data.result[i].startTimeSeconds;
+            var phase = data.result[i].phase;
+
+            if (name.length > 1 && startTime > 0 && phase !== "BEFORE") {
+                var item = {
+                    contestId: parseInt(id),
+                    name: name,
+                    startTime: parseInt(startTime)
+                };
+
+                contests.push(item);
+            }
+        }
+
+        contests.sort(function (a, b) {
+            if (a.startTime > b.startTime) return -1;
+            if (a.startTime < b.startTime) return 1;
+            return 0;
+        });
+
+        callback(contests);
+    });
+}
+
+
+getContestList(function (contests) {
+    var select = document.getElementById("selectContest");
+
+    for (var i = 0; i < contests.length; i++) {
+        var option = document.createElement('option');
+
+        option.setAttribute('value', contests[i].contestId);
+        option.appendChild(document.createTextNode(contests[i].name));
+
+        select.appendChild(option);
+    }
+});
